0519f16a9a2f9a32b52b171f67a25771db61b9d6
metamorphogenesis <w.shuminski@gmail.com>
2021-03-23 21:55:33 +0200

    layouts refactored

--- app/src/main/java/com/example/scaler/MainActivity.java
+++ app/src/main/java/com/example/scaler/MainActivity.java
@@ -11,10 +11,11 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
-    private static double inMin, inMax, outMin, outMax, in, out;
-    private static TextView inputText, outputText;
-    private static EditText inMinField, inMaxField, outMinField, outMaxField;
-    private static SeekBar seekBar;
+    private static double inMin, inMax, outMin, outMax, in;
+    private static boolean inMinErr, inMaxErr, outMinErr, outMaxErr;
+    private TextView inputText, outputText;
+    private EditText inMinField, inMaxField, outMinField, outMaxField;
+    private SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +30,7 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         outputText = findViewById(R.id.outputText);
         seekBar = findViewById(R.id.inputValueBar);
 
-        final SeekBar inputValueBar = findViewById(R.id.inputValueBar);
+        final SeekBar inputValueBar = seekBar;
         inputValueBar.setMin(0);
         inputValueBar.setMax(1000);
         inputValueBar.setOnSeekBarChangeListener(this);
@@ -51,10 +52,7 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
 
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
-            try {
-                calculateInput();
-            } catch (Exception e) {
-            }
+            calculateInput();
         }
 
         @Override
@@ -71,10 +69,7 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
 
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
-            try {
-                scale();
-            } catch (Exception e) {
-            }
+            scale();
         }
 
         @Override
@@ -85,8 +80,10 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
 
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        calculateInput();
-        scale();
+        if (!inMinErr && !inMaxErr && !outMinErr && !outMaxErr) {
+            calculateInput();
+            scale();
+        }
     }
 
     @Override
@@ -97,7 +94,7 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     public void onStopTrackingTouch(SeekBar seekBar) {
     }
 
-    public static void calculateInput() {
+    public void calculateInput() {
         try {
             if (inMinField.getText().length() > 0) {
                 inMinField.setTextSize(18);
@@ -106,8 +103,11 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
             }
 
             inMin = Double.parseDouble(String.valueOf(inMinField.getText()));
+            inMinErr = false;
         } catch (Exception e) {
+            inMinErr = true;
             inMinField.requestFocus();
+            inputText.setTextColor(getResources().getColor(R.color.errorColor, getTheme()));
         }
 
         try {
@@ -117,17 +117,20 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
                 inMaxField.setTextSize(14);
             }
 
-
             inMax = Double.parseDouble(String.valueOf(inMaxField.getText()));
+            inMaxErr = false;
         } catch (Exception e) {
+            inMaxErr = true;
             inMaxField.requestFocus();
+            inputText.setTextColor(getResources().getColor(R.color.errorColor, getTheme()));
         }
 
         in = (inMax - inMin) / 1000 * seekBar.getProgress() + inMin;
         inputText.setText(String.format("%.2f", in));
+        inputText.setTextColor(getResources().getColor(R.color.inputColor, getTheme()));
     }
 
-    public static void scale() {
+    public void scale() {
         try {
             if (outMinField.getText().length() > 0) {
                 outMinField.setTextSize(18);
@@ -136,8 +139,11 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
             }
 
             outMin = Double.parseDouble(String.valueOf(outMinField.getText()));
+            outMinErr = false;
         } catch (Exception e) {
+            outMaxErr = true;
             outMinField.requestFocus();
+            outputText.setTextColor(getResources().getColor(R.color.errorColor, getTheme()));
         }
 
         try {
@@ -147,14 +153,17 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
                 outMaxField.setTextSize(14);
             }
 
-
             outMax = Double.parseDouble(String.valueOf(outMaxField.getText()));
+            outMaxErr = false;
         } catch (Exception e) {
+            outMaxErr = true;
             outMaxField.requestFocus();
+            outputText.setTextColor(getResources().getColor(R.color.errorColor, getTheme()));
         }
 
-        out = (outMax - outMin) / (inMax - inMin) * (in - inMin) + outMin;
+        double out = (outMax - outMin) / (inMax - inMin) * (in - inMin) + outMin;
         outputText.setText(String.format("%.2f", out));
+        outputText.setTextColor(getResources().getColor(R.color.outputColor, getTheme()));
     }
 
 }
