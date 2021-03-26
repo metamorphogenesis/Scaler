c1d20831843c76c1f7b05eae2fef003b085945e3
metamorphogenesis <w.shuminski@gmail.com>
2021-03-26 22:15:56 +0200

    + added input range caution and NaN-output protection; * fixed some bugs

--- app/src/main/java/com/example/scaler/MainActivity.java
+++ app/src/main/java/com/example/scaler/MainActivity.java
@@ -1,26 +1,33 @@
 package com.example.scaler;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintSet;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
-    private static double inMin, inMax, outMin, outMax, in;
+    private static double inMin, inMax, outMin, outMax, in, out;
     private static boolean inMinErr, inMaxErr, outMinErr, outMaxErr;
-    private TextView inputText, outputText;
+    private TextView inputText, outputText, inputCaution, inputValueText;
     private EditText inMinField, inMaxField, outMinField, outMaxField;
-    private SeekBar seekBar;
+    private SeekBar inputValueBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
         inMinField = findViewById(R.id.inMinField);
         inMaxField = findViewById(R.id.inMaxField);
@@ -28,10 +35,9 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         outMaxField = findViewById(R.id.outMaxField);
         inputText = findViewById(R.id.inputText);
         outputText = findViewById(R.id.outputText);
-        seekBar = findViewById(R.id.inputValueBar);
-
-        final SeekBar inputValueBar = seekBar;
-        inputValueBar.setMin(0);
+        inputCaution = findViewById(R.id.inputCaution);
+        inputValueText = findViewById(R.id.inputValueLabel);
+        inputValueBar = findViewById(R.id.inputValueBar);
         inputValueBar.setMax(1000);
         inputValueBar.setOnSeekBarChangeListener(this);
 
@@ -40,31 +46,27 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         outMinField.addTextChangedListener(outputTextWatcher);
         outMaxField.addTextChangedListener(outputTextWatcher);
 
-        calculateInput();
-        scale();
+        calculate();
     }
 
     TextWatcher inputTextWatcher = new TextWatcher() {
         @Override
         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
         }
 
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
-            calculateInput();
+            getInput();
         }
 
         @Override
         public void afterTextChanged(Editable s) {
-
         }
     };
 
     TextWatcher outputTextWatcher = new TextWatcher() {
         @Override
         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
         }
 
         @Override
@@ -74,15 +76,17 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
 
         @Override
         public void afterTextChanged(Editable s) {
-
         }
     };
 
+    //TODO: fix output refreshing when input get fixed
+    //TODO: hide soft keyboard when tap out of the field
+    //TODO: change app icon
+    //TODO: make splashscreen
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
         if (!inMinErr && !inMaxErr && !outMinErr && !outMaxErr) {
-            calculateInput();
-            scale();
+            calculate();
         }
     }
 
@@ -94,76 +98,98 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     public void onStopTrackingTouch(SeekBar seekBar) {
     }
 
-    public void calculateInput() {
-        try {
-            if (inMinField.getText().length() > 0) {
-                inMinField.setTextSize(18);
-            } else {
-                inMinField.setTextSize(14);
-            }
+    private void getInput() {
+        if (inMinField.getText().length() > 0) {
+            inMinField.setTextSize(18);
+        } else {
+            inMinField.setTextSize(14);
+        }
 
+        if (inMaxField.getText().length() > 0) {
+            inMaxField.setTextSize(18);
+        } else {
+            inMaxField.setTextSize(14);
+        }
+
+        try {
             inMin = Double.parseDouble(String.valueOf(inMinField.getText()));
             inMinErr = false;
         } catch (Exception e) {
             inMinErr = true;
             inMinField.requestFocus();
-            inputText.setTextColor(getResources().getColor(R.color.errorColor, getTheme()));
         }
 
         try {
-            if (inMaxField.getText().length() > 0) {
-                inMaxField.setTextSize(18);
-            } else {
-                inMaxField.setTextSize(14);
-            }
-
             inMax = Double.parseDouble(String.valueOf(inMaxField.getText()));
             inMaxErr = false;
         } catch (Exception e) {
             inMaxErr = true;
             inMaxField.requestFocus();
-            inputText.setTextColor(getResources().getColor(R.color.errorColor, getTheme()));
         }
 
-        in = (inMax - inMin) / 1000 * seekBar.getProgress() + inMin;
+        if (inMax == inMin) {
+            inMaxErr = true;
+            in = inMax;
+            inMaxField.requestFocus();
+            inputCaution.setVisibility(View.VISIBLE);
+        } else {
+            in = (inMax - inMin) / 1000 * inputValueBar.getProgress() + inMin;
+            inputCaution.setVisibility(View.INVISIBLE);
+        }
+
         inputText.setText(String.format("%.2f", in));
-        inputText.setTextColor(getResources().getColor(R.color.inputColor, getTheme()));
+
+        if (inMinErr || inMaxErr) {
+            inputText.setTextColor(getResources().getColor(R.color.errorColor, getTheme()));
+        } else {
+            inputText.setTextColor(getResources().getColor(R.color.inputColor, getTheme()));
+        }
     }
 
-    public void scale() {
-        try {
-            if (outMinField.getText().length() > 0) {
-                outMinField.setTextSize(18);
-            } else {
-                outMinField.setTextSize(14);
-            }
+    private void scale() {
+        if (outMinField.getText().length() > 0) {
+            outMinField.setTextSize(18);
+        } else {
+            outMinField.setTextSize(14);
+        }
 
+        if (outMaxField.getText().length() > 0) {
+            outMaxField.setTextSize(18);
+        } else {
+            outMaxField.setTextSize(14);
+        }
+
+        try {
             outMin = Double.parseDouble(String.valueOf(outMinField.getText()));
             outMinErr = false;
         } catch (Exception e) {
-            outMaxErr = true;
+            outMinErr = true;
             outMinField.requestFocus();
-            outputText.setTextColor(getResources().getColor(R.color.errorColor, getTheme()));
         }
 
         try {
-            if (outMaxField.getText().length() > 0) {
-                outMaxField.setTextSize(18);
-            } else {
-                outMaxField.setTextSize(14);
-            }
-
             outMax = Double.parseDouble(String.valueOf(outMaxField.getText()));
             outMaxErr = false;
         } catch (Exception e) {
             outMaxErr = true;
             outMaxField.requestFocus();
-            outputText.setTextColor(getResources().getColor(R.color.errorColor, getTheme()));
         }
 
-        double out = (outMax - outMin) / (inMax - inMin) * (in - inMin) + outMin;
-        outputText.setText(String.format("%.2f", out));
-        outputText.setTextColor(getResources().getColor(R.color.outputColor, getTheme()));
+        if (!inMinErr && !inMaxErr) {
+            out = (outMax - outMin) / (inMax - inMin) * (in - inMin) + outMin;
+            outputText.setText(String.format("%.2f", out));
+        }
+
+        if (outMinErr || outMaxErr) {
+            outputText.setTextColor(getResources().getColor(R.color.errorColor, getTheme()));
+        } else {
+            outputText.setTextColor(getResources().getColor(R.color.outputColor, getTheme()));
+        }
+    }
+
+    public void calculate() {
+        getInput();
+        scale();
     }
 
 }
